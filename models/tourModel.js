1200b869191df01255a65b8272bfd7c802245b4b
Gagan Singh <gagansingh.dev@outlook.com>
2020-08-13 22:34:16 -0400

    Updated tour model to include all fields for production db

--- models/tourModel.js
+++ models/tourModel.js
@@ -2,19 +2,56 @@ const mongoose = require("mongoose");
 
 // Define Schema
 const tourSchema = new mongoose.Schema({
-	name   : {
+	name            : {
 		type     : String,
 		required : [ true, "A tour must have a name" ],
-		unique   : true
+		unique   : true,
+		trim     : true
 	},
-	rating : {
+	duration        : {
+		type     : Number,
+		required : [ true, "A tour must have a duration" ]
+	},
+	maxGroupSize    : {
+		type     : Number,
+		required : [ true, "A tour must have a group size" ]
+	},
+	difficulty      : {
+		type     : String,
+		required : [ true, "A tour should have a difficulty" ]
+	},
+	ratingAverage   : {
 		type    : Number,
 		default : 4.5
 	},
-	price  : {
+	ratingsQuantity : {
+		type    : Number,
+		default : 0
+	},
+	price           : {
 		type     : Number,
 		required : [ true, "A tour must have a price" ]
-	}
+	},
+	priceDiscount   : Number,
+	summary         : {
+		type     : String,
+		required : [ true, "A tour must have a summary" ],
+		trim     : true // remove all white space from begining and end
+	},
+	description     : {
+		type : String,
+		trim : true
+	},
+	imageCover      : {
+		type     : String,
+		required : [ true, "A tour must have a cover image" ]
+	},
+	images          : [ String ], // An array of strings
+	createdAt       : {
+		type    : Date,
+		default : Date.now()
+	},
+	startDate       : [ Date ] // An array of dates
 });
 
 // Mongo Model
